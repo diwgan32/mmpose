10e7597ddfa8c7c7f08e9e67a7d7b03829b4e298
Diwakar Ganesan <diwakar.ganesan@gmail.com>
2021-11-18 11:29:51 -0800

    fix indentation

--- mmpose/datasets/datasets/body3d/body3d_aist_dataset.py
+++ mmpose/datasets/datasets/body3d/body3d_aist_dataset.py
@@ -425,24 +425,24 @@ class Body3DAISTDataset(Kpt3dSviewKpt2dDataset):
 
     @staticmethod
     def rodrigues_vec_to_rotation_mat(rodrigues_vec):
-    theta = np.linalg.norm(rodrigues_vec)
-    if theta < sys.float_info.epsilon:              
-        rotation_mat = np.eye(3, dtype=float)
-    else:
-        r = rodrigues_vec / theta
-        I = np.eye(3, dtype=float)
-        r_rT = np.array([
-            [r[0]*r[0], r[0]*r[1], r[0]*r[2]],
-            [r[1]*r[0], r[1]*r[1], r[1]*r[2]],
-            [r[2]*r[0], r[2]*r[1], r[2]*r[2]]
-        ])
-        r_cross = np.array([
-            [0, -r[2], r[1]],
-            [r[2], 0, -r[0]],
-            [-r[1], r[0], 0]
-        ])
-        rotation_mat = math.cos(theta) * I + (1 - math.cos(theta)) * r_rT + math.sin(theta) * r_cross
-    return rotation_mat 
+        theta = np.linalg.norm(rodrigues_vec)
+        if theta < sys.float_info.epsilon:              
+            rotation_mat = np.eye(3, dtype=float)
+        else:
+            r = rodrigues_vec / theta
+            I = np.eye(3, dtype=float)
+            r_rT = np.array([
+                [r[0]*r[0], r[0]*r[1], r[0]*r[2]],
+                [r[1]*r[0], r[1]*r[1], r[1]*r[2]],
+                [r[2]*r[0], r[2]*r[1], r[2]*r[2]]
+            ])
+            r_cross = np.array([
+                [0, -r[2], r[1]],
+                [r[2], 0, -r[0]],
+                [-r[1], r[0], 0]
+            ])
+            rotation_mat = math.cos(theta) * I + (1 - math.cos(theta)) * r_rT + math.sin(theta) * r_cross
+        return rotation_mat 
 
     def _load_camera_param(self, camera_param_file):
         camera_params = {}
