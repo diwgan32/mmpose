65c7c71c51499a7c90eb8940605280cc1133fc34
Diwakar Ganesan <diwakar.ganesan@gmail.com>
2021-11-18 10:11:59 -0800

    add aist imports

--- mmpose/datasets/datasets/body3d/__init__.py
+++ mmpose/datasets/datasets/body3d/__init__.py
@@ -3,7 +3,10 @@ from .body3d_h36m_dataset import Body3DH36MDataset
 from .body3d_mpi_inf_3dhp_dataset import Body3DMpiInf3dhpDataset
 from .body3d_semi_supervision_dataset import Body3DSemiSupervisionDataset
 from .body3d_h36m_modified_dataset import Body3DH36MModifiedDataset
+from .body3d_aist_dataset import Body3DAISTDataset
+
 __all__ = [
     'Body3DH36MDataset', 'Body3DSemiSupervisionDataset',
-    'Body3DMpiInf3dhpDataset', 'Body3DH36MModifiedDataset'
+    'Body3DMpiInf3dhpDataset', 'Body3DH36MModifiedDataset',
+    'Body3DAISTDataset'
 ]
